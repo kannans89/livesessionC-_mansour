fadf5a3d0ad2a04288322eb4fcdd11a0e4be8c70
kannan <kannan.mumbai@yahoo.com>
2024-07-01 16:36:56 +0530

    overloading

---
+++ Day1/02CommandLineArgs/02CommandLineArgs/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _02CommandLineArgs
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			if (args.Length>0) { 
+			
+				foreach(string name in args)
+				{
+					Console.WriteLine("Hello, " + name);
+				}
+			
+			}
+            else
+            {
+                Console.WriteLine("No arguments passed to say hello");
+            }
+        }
+	}
+}
---
+++ Day1/03OverloadingApp/03OverloadingApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _03OverloadingApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			PrintInfo("hello");
+			PrintInfo("#a");
+			PrintInfo('a');
+			PrintInfo(3);
+			PrintInfo(3f);
+
+
+		}
+
+	   static	void PrintInfo(string input)
+		{
+			Console.WriteLine($"(1) printing string value {input}");
+		}
+
+		static void PrintInfo(char input)
+		{
+			Console.WriteLine($"(2) printing character value {input}");
+		}
+
+		static void PrintInfo(float input)
+		{
+			Console.WriteLine($"(3) printing float value {input}");
+		}
+
+		static void PrintInfo(int input)
+		{
+			Console.WriteLine($"(4) printing int value {input}");
+		}
+	}
+}
