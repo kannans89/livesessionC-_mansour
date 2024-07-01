19c5ebd43ade9f0d0f8c66e92db3cf67838e5435
kannan <kannan.mumbai@yahoo.com>
2024-07-01 15:38:23 +0530

    first app

---
+++ Day1/01WelcomeApp/01WelcomeApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Program
+{
+	static void Main()
+	{
+
+		Console.WriteLine("Enter your user name");
+
+		string userName=Console.ReadLine();
+
+		Console.WriteLine($" Hello user {userName} , welcome to C# programming ");
+
+	}
+}
+
