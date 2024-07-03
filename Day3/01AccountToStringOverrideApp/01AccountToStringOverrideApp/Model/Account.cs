16e03c50d1c07d2fc5aa5b74bc1dc9f36e96c2f7
kannan <kannan.mumbai@yahoo.com>
2024-07-03 13:59:39 +0530

    account inhteriance

---
+++ Day3/01AccountToStringOverrideApp/01AccountToStringOverrideApp/Model/Account.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01AccountToStringOverrideApp.Model
+{
+	public class Account
+	{
+		private int _accoutNumber;
+		private string _name;
+		private double _balance=500;
+
+
+		public Account() {
+
+            Console.WriteLine("Account object got created");
+
+        }
+
+		public Account(int accno,string name,double _balance) :this(){
+
+		
+			_accoutNumber = accno;
+			_name = name;
+			this._balance = _balance;
+		}
+
+
+		public string Name {
+
+
+			get {
+				return _name;
+			}
+
+			set { 
+			 if(value.Length>=3)
+				 _name=value;
+                else
+                {
+                    throw new Exception("Name must be greater than 3 characters");
+                }
+
+            }
+		
+		
+		}
+
+
+
+		public void Deposit(double amount)
+		{
+			if (amount > 0)
+			{
+				_balance += amount;
+			}
+			else
+				throw new Exception("amount must be greater than 0");
+		}
+
+
+		public void Withdraw(double amount) {
+
+			if (_balance - amount > 500) { 
+			
+			   _balance -= amount;
+			}
+			else
+				throw new Exception("Insufficient balance");
+
+		
+		}
+
+
+		public double Balance {
+
+			get { 
+			  return _balance;	
+			}
+		}
+
+		public int AccountNumber {
+
+			get {
+				return _accoutNumber;
+			}
+			set {
+				if (value > 0)
+				{
+					_accoutNumber = value;
+				}
+				else
+					throw new Exception("account no must be greated than 0");
+			}
+		}
+
+	}
+}
---
+++ Day3/01AccountToStringOverrideApp/01AccountToStringOverrideApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using _01AccountToStringOverrideApp.Model;
+
+namespace _01AccountToStringOverrideApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//CaseStudy1();
+
+			Account obj1 = new Account(101, "Ahmad", 5000);
+
+            Console.WriteLine(obj1.ToString());
+        }
+
+		private static void CaseStudy1()
+		{
+			Account obj1 = new Account(101, "Ahmad", 5000);
+			Account obj2 = new Account(102, "Kannan", 4000);
+
+			Account obj3 = new Account();
+
+			Console.WriteLine("hashcode of obj1 " + obj1.GetHashCode());
+			Console.WriteLine("hashcode of obj2 " + obj2.GetHashCode());
+			Console.WriteLine("hashcode of obj3 " + obj3.GetHashCode());
+		}
+	}
+}
