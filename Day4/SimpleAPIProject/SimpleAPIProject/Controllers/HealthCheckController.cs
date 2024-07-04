9bc2366d2563003e8fc54f2b6609744f5cbf0d4d
kannan <kannan.mumbai@yahoo.com>
2024-07-04 12:42:16 +0530

    api

---
+++ Day4/SimpleAPIProject/SimpleAPIProject/Controllers/HealthCheckController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAPIProject.Controllers
+{
+
+	[Route("api/v1/[controller]")]//Attributes,swagger
+	[ApiController]//Attributes
+	public class HealthCheckController:ControllerBase
+	{
+
+		[HttpGet]
+		public string Get()
+		{
+			return "Healthy and Good to Go...";
+		}
+
+		[HttpPost]
+		public string Post()
+		{
+			return "Post:Healthy and Good to Go...";
+		}
+
+		[HttpPut]
+		public string Put()
+		{
+			return "Put:Healthy and Good to Go...";
+		}
+
+		[HttpDelete]
+		public string Delete()
+		{
+			return "Delete:Healthy and Good to Go...";
+		}
+	}
+}
